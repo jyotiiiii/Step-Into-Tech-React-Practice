2f79569fc7a82cc390edffc4936cf7bd0c8626d9
Jyoti <jyoti.contact@gmail.com>
2020-07-16 16:11:22 +0100

    change Favourites to function component

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import Card from './Card';
-class Favourites extends React.Component {
-  render() {
-    const myFavs = this.props.favList;
-    //create new array of character objects where id is present in the myFavs array
 
-    return (
-      <div className="spacer">
-        <h2>My Favourites</h2>
-        <div className="grid">
-          {myFavs.length > 0 ? (
-            myFavs.map((item) => (
-              <Card
-                onHeartClick={this.props.onHeartClick}
-                {...item}
-                key={item.id}
-              />
-            ))
-          ) : (
+function Favourites(props) {
+  const { favList, onHeartClick } = props;
+
+  return (
+    <div className="spacer">
+      <h2>My Favourites</h2>
+      <div className="grid">
+        {favList.length > 0 ? (
+          favList.map((item) => (
+            <Card
+              onHeartClick={onHeartClick}
+              {...item}
+              key={item.id}
+            />
+          ))
+        ) : (
             <small>
               No favourites here yet
               <span role="img" aria-label="sad face">
@@ -25,10 +24,10 @@ class Favourites extends React.Component {
               </span>
             </small>
           )}
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default Favourites;
