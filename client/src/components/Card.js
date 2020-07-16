f5c91681a29ccd7f9baad251f35ad73cb594606e
Jyoti <jyoti.contact@gmail.com>
2020-07-16 16:13:10 +0100

    change card to function component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,54 +1,55 @@
 import React from 'react';
 
-class Card extends React.Component {
-  render() {
-    const {
-      id,
-      characterName,
-      actorName,
-      bio,
-      imageUrl,
-      onHeartClick,
-    } = this.props;
-    return (
-      <div className="card">
-        <div className="container">
-          <img className="fade-in image" src={imageUrl} alt={characterName} />
-          <div className="overlay">
-            <button
-              data-title="Click to add to favourites"
-              onClick={() => onHeartClick(id)}
-              href="#"
-              className="icon"
-              title={`Click to add/remove ${characterName} to favourites`}
-            >
-              <i className="far fa-heart fa-lg"></i>
-            </button>
-          </div>
-        </div>
-        <div className="details">
+function Card(props) {
+
+  const {
+    id,
+    characterName,
+    actorName,
+    bio,
+    imageUrl,
+    onHeartClick,
+  } = props;
+
+  return (
+    <div className="card">
+      <div className="container">
+        <img className="fade-in image" src={imageUrl} alt={characterName} />
+        <div className="overlay">
           <button
-            className="favourite-button positioned-button"
             data-title="Click to add to favourites"
             onClick={() => onHeartClick(id)}
+            href="#"
+            className="icon"
             title={`Click to add/remove ${characterName} to favourites`}
           >
             <i className="far fa-heart fa-lg"></i>
           </button>
-          <h3 className="character">{characterName}</h3>
-
-          <small>
-            <strong>Played by:</strong> {actorName}
-          </small>
-          {bio ? (
-            <small>
-              <strong>Biography:</strong> {bio.substring(0, 80)}...
-            </small>
-          ) : null}
         </div>
       </div>
-    );
-  }
+      <div className="details">
+        <button
+          className="favourite-button positioned-button"
+          data-title="Click to add to favourites"
+          onClick={() => onHeartClick(id)}
+          title={`Click to add/remove ${characterName} to favourites`}
+        >
+          <i className="far fa-heart fa-lg"></i>
+        </button>
+        <h3 className="character">{characterName}</h3>
+
+        <small>
+          <strong>Played by:</strong> {actorName}
+        </small>
+        {bio ? (
+          <small>
+            <strong>Biography:</strong> {bio.substring(0, 80)}...
+          </small>
+        ) : null}
+      </div>
+    </div>
+  );
 }
+
 
 export default Card;
